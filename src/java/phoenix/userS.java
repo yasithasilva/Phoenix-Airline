22c68d640afcd9533356cc40a1817bdc4275adb8
raveena00 <100334856+raveena00@users.noreply.github.com>
2022-10-07 15:31:24 +0530

    add code

--- src/java/phoenix/userS.java
+++ src/java/phoenix/userS.java
@@ -33,7 +33,7 @@ public class userS extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
+            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
