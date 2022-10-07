d8bb93f581246bb99ee9c9659f53238bc504de54
raveena00 <100334856+raveena00@users.noreply.github.com>
2022-10-07 16:51:18 +0530

    update

--- src/java/phoenix/userS.java
+++ src/java/phoenix/userS.java
@@ -7,7 +7,7 @@ package phoenix;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
