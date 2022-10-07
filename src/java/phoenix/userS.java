a5502dd9c5f262f80cc17d0955c50f34734a14d8
raveena00 <100334856+raveena00@users.noreply.github.com>
2022-10-07 16:50:44 +0530

    update

--- src/java/phoenix/userS.java
+++ src/java/phoenix/userS.java
@@ -1,13 +1,13 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @param response servlet respons
  */
 package phoenix;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
